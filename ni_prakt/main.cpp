d0eb66c7a77d7cfc2703011abdeba9c49b5955c5
Ilya Popovnin <itdbnice@gmail.com>
2019-09-26 14:46:48 +0300

    Initial Commit

---
+++ ni_prakt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ni_prakt
+//
+//  Created by Ilya Popovnin on 2019-09-26.
+//  Copyright © 2019 Ilya Popovnin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
